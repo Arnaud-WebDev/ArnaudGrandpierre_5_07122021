e65aae550c672e2e7e69259078b384071fd4af1e
Arnaud-WebDev <arnaud.grandpierre@gmail.com>
2021-12-29 18:40:25 +0100

    création de la fonction renderProducts pour afficher les produits sur la page d'accueil et on relie le tout avec un fetch

---
+++ front/js/products.js
@@ -0,0 +1,5 @@
+fetch('http://localhost:3000/api/products');
+/* .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => console.log(error)); */
---
+++ front/js/script.js
@@ -0,0 +1,21 @@
+// Rendu des produits
+function renderProducts(products) {
+  console.log(products);
+  products.forEach((product) => {
+    console.log(product);
+    const html = `<a href="./product.html?id=${product._id}">
+            <article>
+              <img src="${product.imageUrl}" alt="${product.altTxt} />
+              <h3 class="productName">${product.name}</h3>
+              <p class="productDescription">${product.description}</p>
+            </article>
+          </a>`;
+    document.getElementById('items').innerHTML += html;
+  });
+}
+
+fetch('http://localhost:3000/api/products')
+  .then((response) => response.json())
+  .then((data) => {
+    renderProducts(data);
+  });
