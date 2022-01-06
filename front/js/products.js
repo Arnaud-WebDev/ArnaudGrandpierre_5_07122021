d5e38df5908991d80b163490b3cd51733e53f1aa
Arnaud-WebDev <arnaud.grandpierre@gmail.com>
2022-01-06 09:51:54 +0100

    Affichage du prix, description etc..... d'une page produit, via getElementById et pour la photo via foreach

--- front/js/products.js
+++
@@ -1,5 +0,0 @@
-fetch('http://localhost:3000/api/products');
-/* .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => console.log(error)); */
--- front/js/script.js
+++ front/js/script.js
@@ -1,5 +1,5 @@
 // Rendu des produits
-function renderProducts(products) {
+/* function renderProducts(products) {
   console.log(products);
   products.forEach((product) => {
     console.log(product);
@@ -19,3 +19,25 @@ fetch('http://localhost:3000/api/products')
   .then((data) => {
     renderProducts(data);
   });
+ */
+
+fetch('http://localhost:3000/api/products')
+  .then((response) => response.json())
+  .then((data) => {
+    renderProducts(data);
+  });
+
+function renderProducts(products) {
+  console.log(products);
+  products.forEach((product) => {
+    console.log(product);
+    const html = ` <a href="./product.html?id=${product._id}">
+            <article>
+              <img src="${product.imageUrl}" alt="${product.altTxt} />
+              <h3 class="productName">${product.name}</h3>
+              <p class="productDescription">${product.description}</p>
+            </article>
+          </a>`;
+    document.getElementById('items').innerHTML += html;
+  });
+}
