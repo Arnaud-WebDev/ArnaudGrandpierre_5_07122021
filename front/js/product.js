9af8edb509e0657af8a3db91cd2edba717e763fd
Arnaud-WebDev <arnaud.grandpierre@gmail.com>
2022-01-19 13:35:40 +0100

    Ajout d'une id dans la balise title pour changer le nom de l'en-tête à chaque changement de page et ajout de produit dans le panier grâce à 'localStorage'

--- front/js/product.js
+++ front/js/product.js
@@ -1,179 +1,58 @@
 let searchParams = new URLSearchParams(window.location.search); //Permet de travailler avec la chaîne de requête d'une URL
-console.log(searchParams);
+//console.log(searchParams);
 let id = searchParams.get('id'); //Récupère l'id de la page
-console.log(id);
+//console.log(id);
 
 const dataApi = fetch(`http://localhost:3000/api/products/${id}`);
 
 //Affichage du prix, description etc ....
 dataApi.then(async (responseData) => {
-  console.log(responseData);
+  //console.log(responseData);
 
-  const response = await responseData.json();
-  console.log(response);
+  let response = await responseData.json();
+  //console.log(response);
 
-  const title = (document.getElementById('title').innerHTML = response.name);
-  console.log(title);
+  const title = (document.getElementById('title').innerHTML = response.name); //Donne le nom du produit
+  //console.log(title);
 
-  const price = (document.getElementById('price').innerHTML = response.price);
-  console.log(price);
+  const price = (document.getElementById('price').innerHTML = response.price); //Donne le prix du produit
+  // console.log(price);
 
-  const description = (document.getElementById('description').innerHTML = response.description);
-  console.log(description);
+  const description = (document.getElementById('description').innerHTML = response.description); //Récupère la description du produit
+  //console.log(description);
 
-  document.querySelector('.item__img img').setAttribute('src', response.imageUrl);
+  document.querySelector('.item__img img').setAttribute('src', response.imageUrl); //Récupère l'image du produit
 
   response.colors.forEach((color) => {
-    console.log(color);
-    console.log(`<option value="${color}">${color}</option>`);
-    document.getElementById('colors').innerHTML += `<option value="${color}">${color}</option>`;
+    // console.log(color);
+    // console.log(`<option value="${color}">${color}</option>`);
+    document.getElementById('colors').innerHTML += `<option value="${color}">${color}</option>`; //Permet d'afficher chaque couleurs des produits
+  });
+  document.getElementById('title_page').innerHTML = response.name; //Change le nom de l'entête de la page
+
+  //---------------------- Bouton avec addEventListener ------------------------------
+  const bouton = document.getElementById('addToCart');
+  bouton.addEventListener('click', () => {
+    //Déclaration de la variable 'productInCart' dans laquelle on met les key et les values qui sont dans le local storage :
+    let productInCart = JSON.parse(localStorage.getItem('product'));
+    //console.log(productInCart);
+    //JSON.parse c'est pour convertir les données au format JSON qui sont dans le local storage en objet JavaScript
+
+    const addProductInCart = () => {
+      //Ajout dans le tableau de l'objet avec les values choisi par l'utilisateur :
+      productInCart.push(response);
+      //La transformation en format JSON et l'envoyer dans la key 'product' du localStorage :
+      localStorage.setItem('product', JSON.stringify(productInCart));
+    };
+
+    //Si il y a déjà des produits d'enregistré dans le local storage
+    if (productInCart) {
+      addProductInCart();
+    }
+    //Si il n'y a pas de produit d'enregistré dans le local storage
+    else {
+      productInCart = [];
+      addProductInCart();
+    }
   });
 });
-
-//console.log(title);
-
-/* fetch('http://localhost:3000/api/products/${_id}');
-console.log(fetch); */
-/* document.getElementById('title').innerHTML = searchParams.title;  */
-/* const products = [
-  {
-    colors: ['Blue', 'White', 'Black'],
-    _id: '107fb5b75607497b96722bda5b504926',
-    name: 'Kanap Sinopé',
-    price: 1849,
-    imageUrl: 'kanap01.jpeg',
-    description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    altTxt: "Photo d'un canapé bleu, deux places",
-  },
-  {
-    colors: ['Black/Yellow', 'Black/Red'],
-    _id: '415b7cacb65d43b2b5c1ff70f3393ad1',
-    name: 'Kanap Cyllène',
-    price: 4499,
-    imageUrl: 'kanap02.jpeg',
-    description: 'Morbi nec erat aliquam, sagittis urna non, laoreet justo. Etiam sit amet interdum diam, at accumsan lectus.',
-    altTxt: "Photo d'un canapé jaune et noir, quattre places",
-  },
-  {
-    colors: ['Green', 'Red', 'Orange'],
-    _id: '055743915a544fde83cfdfc904935ee7',
-    name: 'Kanap Calycé',
-    price: 3199,
-    imageUrl: 'kanap03.jpeg',
-    description: 'Pellentesque fermentum arcu venenatis ex sagittis accumsan. Vivamus lacinia fermentum tortor.Mauris imperdiet tellus ante.',
-    altTxt: "Photo d'un canapé d'angle, vert, trois places",
-  },
-  {
-    colors: ['Pink', 'White'],
-    _id: 'a557292fe5814ea2b15c6ef4bd73ed83',
-    name: 'Kanap Autonoé',
-    price: 1499,
-    imageUrl: 'kanap04.jpeg',
-    description: 'Donec mattis nisl tortor, nec blandit sapien fermentum at. Proin hendrerit efficitur fringilla. Lorem ipsum dolor sit amet.',
-    altTxt: "Photo d'un canapé rose, une à deux place",
-  },
-  {
-    colors: ['Grey', 'Purple', 'Blue'],
-    _id: '8906dfda133f4c20a9d0e34f18adcf06',
-    name: 'Kanap Eurydomé',
-    price: 2249,
-    imageUrl: 'kanap05.jpeg',
-    description: 'Ut laoreet vulputate neque in commodo. Suspendisse maximus quis erat in sagittis. Donec hendrerit purus at congue aliquam.',
-    altTxt: "Photo d'un canapé gris, trois places",
-  },
-  {
-    colors: ['Grey', 'Navy'],
-    _id: '77711f0e466b4ddf953f677d30b0efc9',
-    name: 'Kanap Hélicé',
-    price: 999,
-    imageUrl: 'kanap06.jpeg',
-    description: 'Curabitur vel augue sit amet arcu aliquet interdum. Integer vel quam mi. Morbi nec vehicula mi, sit amet vestibulum.',
-    altTxt: "Photo d'un canapé gris, deux places",
-  },
-  {
-    colors: ['Red', 'Silver'],
-    _id: '034707184e8e4eefb46400b5a3774b5f',
-    name: 'Kanap Thyoné',
-    price: 1999,
-    imageUrl: 'kanap07.jpeg',
-    description: 'EMauris imperdiet tellus ante, sit amet pretium turpis molestie eu. Vestibulum et egestas eros. Vestibulum non lacus orci.',
-    altTxt: "Photo d'un canapé rouge, deux places",
-  },
-  {
-    colors: ['Pink', 'Brown', 'Yellow', 'White'],
-    _id: 'a6ec5b49bd164d7fbe10f37b6363f9fb',
-    name: 'Kanap orthosie',
-    price: 3999,
-    imageUrl: 'kanap08.jpeg',
-    description: 'Mauris molestie laoreet finibus. Aenean scelerisque convallis lacus at dapibus. Morbi imperdiet enim metus rhoncus.',
-    altTxt: "Photo d'un canapé rose, trois places",
-  },
-]; */
-
-/* // Utilisation de la méthode .find
-const idProduct = products.find((element) => element._id === id);
-console.log(idProduct);
-
-// Sélection de la classe dans laquelle on va injecter le code html
-const positionElement = document.querySelector('.item');
-console.log(positionElement);
-
-// La structure html pour l'affichage du produit selectionné
-const structureProduit = `<article>
-            <div class="item__img">
-              <img src="${idProduct.imageUrl}" />
-            </div>
-            <div class="item__content">
-              <div class="item__content__titlePrice">
-                <h1 id="title">${idProduct.name}</h1>
-                <p>Prix : <span id="price"> ${idProduct.price} </span>€</p>
-              </div>
-
-              <div class="item__content__description">
-                <p class="item__content__description__title">Description :</p>
-                <p id="description">${idProduct.description}</p>
-              </div>
-
-              <div class="item__content__settings">
-                <div class="item__content__settings__color">
-                  <label for="color-select">Choisir une couleur :</label>
-                  <select name="color-select" id="colors">
-                    <option value="">--SVP, choisissez une couleur --</option>
-                    <option value="vert">vert</option>
-                    <option value="blanc">blanc</option>
-                  </select>
-                </div>
-
-                <div class="item__content__settings__quantity">
-                  <label for="itemQuantity">Nombre d'article(s) (1-100) :</label>
-                  <input type="number" name="itemQuantity" min="1" max="100" value="0" id="quantity" />
-                </div>
-              </div>
-
-              <div class="item__content__addButton">
-                <button id="addToCart">Ajouter au panier</button>
-              </div>
-            </div>
-          </article>
-          `;
-
-//Injection HTML
-
-positionElement.innerHTML = structureProduit; */
-
-// let response = fetch('http://localhost:3000/api/products/${id}');
-/* const image = params.get('item_img');
-const title = params.get('title');
-const price = params.get('price');
-const description = params.get('description');
-
-console.log(title);
- */
-/* const queryString_url_id = window.location.search;
-console.log(queryString_url_id);
-
-const ID = queryString_url_id.slice(1);
-console.log(ID);
- */
-
-// Utilisation de la méthode .find
--- front/js/script.js
+++ front/js/script.js
@@ -1,26 +1,3 @@
-// Rendu des produits
-/* function renderProducts(products) {
-  console.log(products);
-  products.forEach((product) => {
-    console.log(product);
-    const html = `<a href="./product.html?id=${product._id}">
-            <article>
-              <img src="${product.imageUrl}" alt="${product.altTxt} />
-              <h3 class="productName">${product.name}</h3>
-              <p class="productDescription">${product.description}</p>
-            </article>
-          </a>`;
-    document.getElementById('items').innerHTML += html;
-  });
-}
-
-fetch('http://localhost:3000/api/products')
-  .then((response) => response.json())
-  .then((data) => {
-    renderProducts(data);
-  });
- */
-
 fetch('http://localhost:3000/api/products')
   .then((response) => response.json())
   .then((data) => {
@@ -28,9 +5,9 @@ fetch('http://localhost:3000/api/products')
   });
 
 function renderProducts(products) {
-  console.log(products);
+  //console.log(products);
   products.forEach((product) => {
-    console.log(product);
+    // console.log(product);
     const html = ` <a href="./product.html?id=${product._id}">
             <article>
               <img src="${product.imageUrl}" alt="${product.altTxt} />
